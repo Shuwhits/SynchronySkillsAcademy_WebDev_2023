0bb982a17094f960788edf5183950846723ba565
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 12:00:09 -0400

    Create introToJavaScript.js

---
+++ Module3-Javascript/introToJavaScript.js
@@ -0,0 +1,30 @@
+// READY FOR YOUR FIRST LINES OF JAVASCRIPT !!
+// GET STARTED BELOW AND ASK QUESTIONS IF YOU GET STUCK 😉
+// Begin here ⬇️ make sure you follow along the lessonplan.md
+
+// Excercise ①❗️
+var CHARLIE;
+
+
+
+// Excercise ②❗️
+CHARLIE = 6;
+
+
+
+// Excercise ③❗️
+console.log(CHARLIE);
+
+const code = "Coding"; // this variable is constant
+const fact = "is cool!";   // this variable is constant
+
+// Uncomment the constant variable we are attempting to reassign below ⬇️
+// fact = "is awesome!";
+
+// log both variables onto the console here ⬇️ 
+
+
+
+
+
+// WHAT HAPPENS IF YOU TRY TO LOG THE RE-ASSIGNED VARIABLE TO THE CONSOLE ?
