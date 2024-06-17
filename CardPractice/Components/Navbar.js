692dc186aa0e3ac81bf1ead9d375a30df3166a0d
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 12:19:40 -0400

    Create Navbar.js

---
+++ CardPractice/Components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
