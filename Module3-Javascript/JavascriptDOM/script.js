dc4c0ca1822f9985973fff9ebd05b2a9b3aafa52
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-06-17 12:01:38 -0400

    Create script.js

---
+++ Module3-Javascript/JavascriptDOM/script.js
@@ -0,0 +1,19 @@
+// This is a comment
+//Change this alert to say "Welcome to JavaScript"
+// alert("Welcome to JavaScript");
+
+//using console log
+console.log("This will print to the console")
+
+// Uncomment the code below 👇 this comment and change the h1 tag to say "Hello DAE"
+// document.getElementById("title").innerHTML = "";
+
+
+document.getElementsByClassName("title").innerHTML = "MoMoney"
+
+
+function changeText() {
+  document.getElementById("added").innerHTML = "Item added to cart ✅"
+}
+
+
